1a58ac66f34bc7c6249e8fa07ee141f5f9e24f2a
Ryan Martin <rmrt1n@users.noreply.github.com>
2023-07-25 20:50:33 +0800

    feat(web): add api route to push new eliability to sawtooth

--- packages/web/src/routes/(app)/dashboard/sites/[siteId]/create-batch/+page.server.js
+++ packages/web/src/routes/(app)/dashboard/sites/[siteId]/create-batch/+page.server.js
@@ -2,110 +2,114 @@ import { fail } from '@sveltejs/kit';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	default: async ({ request, locals }) => {
-		const body = Object.fromEntries(await request.formData());
-		const { weight, site_id, type, recipient } = body;
-		const emissions = [body.scope1, body.scope2, body.scope3, body.scope4, body.scope5]
-			.map((e, i) => ({ emission_value: e, scope: i + 1 }))
-			.filter((e) => e.emission_value.length > 0)
-			.map((e) => ({ ...e, emission_value: Number(e.emission_value) }));
-		const parentBatches = body.parents
-			.toString()
-			.split(',')
-			.map((e) => Number(e.trim()));
-		const direct_emissions = emissions.reduce((acc, e) => acc + e.emission_value, 0);
-		const total_emissions = direct_emissions;
+  default: async ({ fetch, request, locals }) => {
+    const body = Object.fromEntries(await request.formData());
+    const { weight, site_id, type, recipient } = body;
+    const emissions = [body.scope1, body.scope2, body.scope3, body.scope4, body.scope5]
+      .map((e, i) => ({ emission_value: e, scope: i + 1 }))
+      .filter((e) => e.emission_value.length > 0)
+      .map((e) => ({ ...e, emission_value: Number(e.emission_value) }));
+    const parentBatches = body.parents
+      .toString()
+      .split(',')
+      .map((e) => Number(e.trim()));
+    const direct_emissions = emissions.reduce((acc, e) => acc + e.emission_value, 0);
+    const total_emissions = direct_emissions;
 
-		const { data: batch, error: err } = await locals.supabase
-			.from('batches')
-			.insert({ weight: Number(weight), site_id: Number(site_id), type })
-			.select()
-			.single();
+    const { data: batch, error: err } = await locals.supabase
+      .from('batches')
+      .insert({ weight: Number(weight), site_id: Number(site_id), type })
+      .select()
+      .single();
 
-		if (err) {
-			console.log(err);
-			return fail(500, { error: err.message });
-		}
+    if (err) {
+      console.log(err);
+      return fail(500, { error: err.message });
+    }
 
-		const { data: eliability, error: err2 } = await locals.supabase
-			.from('eliabilities')
-			.insert({
-				batch_id: batch.batch_id,
-				recipient_site_id: Number(recipient),
-				total_emissions,
-				direct_emissions
-			})
-			.select()
-			.single();
+    const { data: eliability, error: err2 } = await locals.supabase
+      .from('eliabilities')
+      .insert({
+        batch_id: batch.batch_id,
+        recipient_site_id: Number(recipient),
+        total_emissions,
+        direct_emissions
+      })
+      .select()
+      .single();
 
-		if (err2) {
-			console.log(err2);
-			return fail(500, { error: err2.message });
-		}
+    if (err2) {
+      console.log(err2);
+      return fail(500, { error: err2.message });
+    }
 
-		const { error: err3 } = await locals.supabase
-			.from('emissions')
-			.insert(emissions.map((e) => ({ ...e, eliability_id: eliability.eliability_id })));
+    const { error: err3 } = await locals.supabase
+      .from('emissions')
+      .insert(emissions.map((e) => ({ ...e, eliability_id: eliability.eliability_id })));
 
-		if (err3) {
-			console.log(err3);
-			return fail(500, { error: err3.message });
-		}
+    if (err3) {
+      console.log(err3);
+      return fail(500, { error: err3.message });
+    }
 
-		// if there's upstream eliabilities, add to eliability_relationships table and update total emissions
-		if (parentBatches) {
-			const { data: parents, error: err4 } = await locals.supabase
-				.from('eliabilities')
-				.select('eliability_id')
-				.in('batch_id', parentBatches);
+    // if there's upstream eliabilities, add to eliability_relationships table and update total emissions
+    if (parentBatches) {
+      const { data: parents, error: err4 } = await locals.supabase
+        .from('eliabilities')
+        .select('eliability_id')
+        .in('batch_id', parentBatches);
 
-			if (err4) {
-				console.log(err4);
-				return fail(400, { error: err4.message });
-			}
+      if (err4) {
+        console.log(err4);
+        return fail(400, { error: err4.message });
+      }
 
-			const { error: err5 } = await locals.supabase
-				.from('eliability_relationships')
-				.insert(
-					parents.map((e) => ({ parent_id: e.eliability_id, child_id: eliability.eliability_id }))
-				);
+      const { error: err5 } = await locals.supabase
+        .from('eliability_relationships')
+        .insert(
+          parents.map((e) => ({ parent_id: e.eliability_id, child_id: eliability.eliability_id }))
+        );
 
-			if (err5) {
-				console.log(err5);
-				return fail(500, { error: err5.message });
-			}
+      if (err5) {
+        console.log(err5);
+        return fail(500, { error: err5.message });
+      }
 
-			// for case of mills producing pk, where the emissions associated with the ffbs required to
-			// produce the pk have been transferred to cpo. this results in the eliability having 0
-			// upstream eliabilities, but you can still view the source of the ffbs.
-			if (!body.moved) {
-				const { data: emissions, error: err6 } = await locals.supabase
-					.from('eliabilities')
-					.select('total_emissions')
-					.in(
-						'eliability_id',
-						parents.map((e) => e.eliability_id)
-					);
+      // for case of mills producing pk, where the emissions associated with the ffbs required to
+      // produce the pk have been transferred to cpo. this results in the eliability having 0
+      // upstream eliabilities, but you can still view the source of the ffbs.
+      if (!body.moved) {
+        const { data: emissions, error: err6 } = await locals.supabase
+          .from('eliabilities')
+          .select('total_emissions')
+          .in(
+            'eliability_id',
+            parents.map((e) => e.eliability_id)
+          );
 
-				if (err6) {
-					console.log(err6);
-					return fail(500, { error: err6.message });
-				}
+        if (err6) {
+          console.log(err6);
+          return fail(500, { error: err6.message });
+        }
 
-				const new_total_emissions =
-					total_emissions + emissions.reduce((acc, e) => acc + e.total_emissions, 0);
-				const { error } = await locals.supabase
-					.from('eliabilities')
-					.update({ total_emissions: new_total_emissions })
-					.eq('eliability_id', eliability.eliability_id);
+        const new_total_emissions =
+          total_emissions + emissions.reduce((acc, e) => acc + e.total_emissions, 0);
+        const { error } = await locals.supabase
+          .from('eliabilities')
+          .update({ total_emissions: new_total_emissions })
+          .eq('eliability_id', eliability.eliability_id);
 
-				if (error) {
-					console.log(error);
-					return fail(500, { error: error.message });
-				}
-			}
-		}
+        if (error) {
+          console.log(error);
+          return fail(500, { error: error.message });
+        }
+      }
+    }
 
-		return { success: true };
-	}
+    // push data to sawtooth
+    const res = await fetch(`/api/push-sawtooth?id=${eliability.eliability_id}`)
+    console.log(res)
+
+    return { success: true };
+  }
 };
---
+++ packages/web/src/routes/api/push-sawtooth/+server.js
@@ -0,0 +1,99 @@
+import { json, error } from '@sveltejs/kit';
+import { createHash } from 'crypto';
+import { CryptoFactory, createContext } from 'sawtooth-sdk-js/signing';
+import { Secp256k1PrivateKey } from 'sawtooth-sdk-js/signing/secp256k1';
+import { protobuf } from 'sawtooth-sdk-js';
+
+import { SAWTOOTH_PRIVATE_KEY, SAWTOOTH_URL } from '$env/static/private';
+
+const hash = (x) => createHash('sha512').update(x).digest('hex');
+
+const GRAPE_FAMILY = 'grape';
+const GRAPE_NAMESPACE = hash(GRAPE_FAMILY).substring(0, 6);
+
+/** @type {import('./$types').RequestHandler} */
+export const POST = async ({ fetch, url, locals }) => {
+  if (!url.searchParams.get('id')) throw error(500, 'error: missing parameter "id"');
+  if (isNaN(url.searchParams.get('id'))) throw error(500, 'error: "id" is not number');
+
+  const id = Number(url.searchParams.get('id'));
+  const {
+    data: { json: payload },
+    error: err
+  } = await locals.supabase
+    .from('eliability_jsons')
+    .select('json')
+    .eq('eliability_id', id)
+    .single();
+
+  if (err) {
+    throw error(500, err.message);
+  }
+
+  const res = await sawtooth_write(`push|${JSON.stringify(payload)}`);
+
+  if (res.status !== 202) {
+    throw error(500, 'error writing to sawtooth state');
+  }
+
+  return json({ success: true, payload });
+};
+
+/**
+ * @param {string} payload
+ * @returns Payload
+ */
+const sawtooth_write = async (payload) => {
+  const context = createContext('secp256k1');
+  const privateKey = Secp256k1PrivateKey.fromHex(SAWTOOTH_PRIVATE_KEY);
+  const signer = new CryptoFactory(context).newSigner(privateKey);
+  const publicKey = signer.getPublicKey().asHex();
+  const address = GRAPE_NAMESPACE + hash(publicKey).substring(0, 64);
+  const encoder = new TextEncoder();
+  const payloadBytes = encoder.encode(payload);
+
+  const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+    familyName: GRAPE_FAMILY,
+    familyVersion: '1.0',
+    inputs: [address],
+    outputs: [address],
+    signerPublicKey: publicKey,
+    nonce: '' + Math.random(),
+    batcherPublicKey: publicKey,
+    dependencies: [],
+    payloadSha512: hash(payloadBytes)
+  }).finish();
+
+  const transaction = protobuf.Transaction.create({
+    header: transactionHeaderBytes,
+    headerSignature: signer.sign(transactionHeaderBytes),
+    payload: payloadBytes
+  });
+
+  const transactions = [transaction];
+
+  const batchHeaderBytes = protobuf.BatchHeader.encode({
+    signerPublicKey: publicKey,
+    transactionIds: transactions.map((e) => e.headerSignature)
+  }).finish();
+
+  const batchSignature = signer.sign(batchHeaderBytes);
+
+  const batch = protobuf.Batch.create({
+    header: batchHeaderBytes,
+    headerSignature: batchSignature,
+    transactions: transactions
+  });
+
+  const batchListBytes = protobuf.BatchList.encode({
+    batches: [batch]
+  }).finish();
+
+  const res = await fetch(`${SAWTOOTH_URL}/batches`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/octet-stream' },
+    body: batchListBytes
+  });
+
+  return res;
+};
