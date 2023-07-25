e47baa9d0e4cf6406523bff8773f3a35bdec0f69
Ryan Martin <rmrt1n@users.noreply.github.com>
2023-07-25 21:13:50 +0800

    fix: errors causing build error

--- packages/processor/index.js
+++ packages/processor/index.js
@@ -11,8 +11,10 @@ if (process.argv.length < 3) {
 const address = process.argv[2];
 const transactionProcessor = new TransactionProcessor(address);
 
+// IMPORTANT: don't add a console.log here to check if it starts. Doing so will for some reason
+// break the transaction processor, and it won't handle any requests.
+// edit*: DON'T ADD ANYTHING UNDER transactionProcessor.start(), event comments. it will break.
+// this might just be happening in my setup, so if anyone were to try and manage to prove me wrong,
+// please send a pull request.
 transactionProcessor.addHandler(new GrapeHandler());
 transactionProcessor.start();
-
-// IMPORTANT: don't add a console.log here to check if it starts. Doing so will for some reason
-// break the transaction processor, and it won't handle any requests. smh
